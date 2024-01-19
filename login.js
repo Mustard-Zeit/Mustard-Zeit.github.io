d0b9cab0b6dc2ccf5f13948c523d09c276ed0b80
Mustard-Zeit <137809930+Mustard-Zeit@users.noreply.github.com>
2024-01-18 21:19:34 -0600

    login.js

--- login.js
+++ login.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoeaded',function(){
   console.log("Hello, world!");
+  var inputEmail = document.getElementyById('email');
+  var inputPassword = document.getElementyById('password');
+  console.log(inputEmail);
+  console.log(inputPassword);
 });
